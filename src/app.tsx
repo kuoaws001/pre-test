561ad7921acf17308e165a488b64d28bf6dee9af
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2023-11-30 21:04:25 +0800

    CustomInputNumber

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import '@/styles/index.css'
+import CustomInputNumber from './components/CustomInputNumber'
 
 const App = () => {
+
+    const handleChange = (e: any) => {
+        const { name, value } = e.target
+        console.log('onChange', name, value);
+    }
+
+    const handleBlur = (e: any) => {
+        const { name, value } = e.target
+        console.log('onBlur', name, value);
+    }
+
     return (
-        <></>
+        <>
+            <CustomInputNumber
+                min={0}
+                max={15}
+                step={2}
+                name='qty'
+                value={1}
+                disabled={false}
+                onChange={handleChange}
+                onBlur={handleBlur}
+            />
+        </>
     )
 }
 
---
+++ src/components/CustomInputNumber.tsx
@@ -0,0 +1,131 @@
+import React, { useState, useRef, useEffect, useReducer } from 'react'
+
+const Action = {
+    increment: 'increment',
+    decrement: 'decrement',
+    set: 'set',
+    stop: 'stop'
+}
+
+interface IProps {
+    min: number;
+    max: number;
+    step: number;
+    name: string;
+    value: number;
+    disabled: boolean;
+    onChange: (e: any) => void;
+    onBlur: (e: any) => void;
+}
+
+const qtyReducer = (min: number, max: number) => {
+    return (state: any, action: any) => {
+        const { type, payload } = action;
+
+        switch (type) {
+            case Action.increment:
+                if (state.qty + payload >= max) {
+                    return { qty: max }
+                } else {
+                    return { qty: state.qty + payload }
+                }
+
+            case Action.decrement:
+                if (state.qty - payload <= min) {
+                    return { qty: min }
+                } else {
+                    return { qty: state.qty - payload }
+                }
+
+            case Action.set:
+                return { qty: payload }
+
+            default:
+                return { qty: state.qty }
+        }
+    }
+}
+
+const CustomInputNumber = ({ min, max, step, name, value, disabled, onChange, onBlur }: IProps) => {
+
+    const [state, dispatch] = useReducer(qtyReducer(min, max), { qty: value });
+    const [action, setAction] = useState('');
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const emitChangeEvent = () => {
+            let e = new Event('change', { bubbles: true })
+            inputRef.current && inputRef.current.dispatchEvent(e)
+            onChange(e)
+        }
+
+        emitChangeEvent();
+    }, [state.qty])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+
+            if (!action) return
+
+            if (action == Action.increment) {
+                dispatch({ type: Action.increment, payload: step })
+            }
+
+            if (action == Action.decrement) {
+                dispatch({ type: Action.decrement, payload: step })
+            }
+
+            if (action == Action.stop) {
+                clearInterval(interval)
+            }
+
+        }, 200)
+
+        return () => clearInterval(interval);
+    }, [action])
+
+    return (
+        <div>
+            <button
+                className={`btn-box ${state.qty <= min || disabled ? 'disable' : ''}`}
+                onMouseDown={() => setAction(Action.decrement)}
+                onMouseUp={() => setAction(Action.stop)}
+                onClick={() => dispatch({ type: Action.decrement, payload: step })}
+                disabled={disabled}
+            >
+                <span>-</span>
+            </button>
+            <input
+                className='number-input-box'
+                type='number'
+                name={name}
+                value={state.qty}
+                ref={inputRef}
+                onChange={(e) => dispatch({ type: Action.set, payload: e.target.value })}
+                onBlur={(e) => {
+                    let value = Number(e.target.value);
+                    if (value >= max) value = max;
+                    if (value <= min) value = min;
+                    dispatch({ type: Action.set, payload: value })
+                    onBlur(e)
+                }}
+                onInput={() => {
+                    console.log('emit InputEvent');
+                }}
+                disabled={disabled}
+            />
+            <button
+                className={`btn-box ${state.qty >= max || disabled ? 'disable' : ''}`}
+                onMouseDown={() => setAction(Action.increment)}
+                onMouseUp={() => setAction(Action.stop)}
+                onClick={() => dispatch({ type: Action.increment, payload: step })}
+                disabled={disabled}
+            >
+                <span>+</span>
+            </button>
+        </div>
+    )
+}
+
+export default CustomInputNumber
